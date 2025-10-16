9a03c4d1d3afeb4b9a47e2e9a9ece82b301091f0
Humberto <hb.lozadas@gmail.com>
2025-10-16 17:52:45 -0500

    Hemos creado el endpoint de tasas

---
+++ src/main/java/com/cambiosmart/demo/spring/boot/tasas/Tasas.java
@@ -0,0 +1,87 @@
+package com.cambiosmart.demo.spring.boot.tasas;
+
+public class Tasas {
+    private Long id;
+    private String moneda;
+    private Double compra;
+    private Double venta;
+    private String fecha;
+    private String fuente;
+    private String hora;
+    private String tipo;
+
+    public Tasas (Long id, String moneda, Double compra, Double venta, String fecha, String fuente, String hora, String tipo) {
+        this.id = id;
+        this.moneda = moneda;
+        this.compra = compra;
+        this.venta = venta;
+        this.fecha = fecha;
+        this.fuente = fuente;
+        this.hora = hora;
+        this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(String moneda) {
+        this.moneda = moneda;
+    }
+
+    public Double getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Double compra) {
+        this.compra = compra;
+    }
+
+    public Double getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Double venta) {
+        this.venta = venta;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getFuente() {
+        return fuente;
+    }
+
+    public void setFuente(String fuente) {
+        this.fuente = fuente;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
